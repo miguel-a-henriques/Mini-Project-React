7154c0d12fe512f27dfc764dc25e59640c6b9bf9
Miguel Henriques <miguelaghenriques@gmail.com>
2024-02-05 18:00:58 +0000

    footer updated

--- recipe-book-app/src/components/Footer.jsx
+++ recipe-book-app/src/components/Footer.jsx
@@ -2,4 +2,13 @@
 function Footer (props) {
 
     // should display the link to the GitHub repository
+    return (
+        <footer id="footer">
+            <a href="https://github.com/miguel-a-henriques/Mini-Project-React">
+            <img src="https://banner2.cleanpng.com/20180615/kff/kisspng-computer-icons-github-logo-github-5b23f7d7474c14.790619341529083863292.jpg" alt="GitHub logo"></img>
+            </a>
+        </footer>
+    )
 }
+
+export default Footer
