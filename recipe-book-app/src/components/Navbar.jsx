abd19f16f622da49d81c076ad4700954a009dab2
Miguel Henriques <miguelaghenriques@gmail.com>
2024-02-07 17:49:05 +0000

    css added to navbar-footer-sidebar

--- recipe-book-app/src/components/Navbar.jsx
+++ recipe-book-app/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar (props) {
     //should display the App's name and logo
 
     return (
-        <header id="header">
+        <header id="header" style={{width: "100%", position: "fixed", top: 0, left: 0, padding: "10px 0", backgroundColor: "orange", display:"flex", justifyContent: "center", alignItems: "center", height: 50}}>
             <h1>Recipe Book APP</h1><img src="" alt="app logo"></img>
         </header>
     )
--- recipe-book-app/src/components/Sidebar.jsx
+++ recipe-book-app/src/components/Sidebar.jsx
@@ -3,9 +3,11 @@ function Sidebar (props) {
 
     // should contain links to Home and About page
     return (
-        <div id="sidebar">
-            <button>Home</button>
-            <button>About</button>
+        <div id="sidebar" style={{height:"100%",position:"fixed", left: 0, overflowX:"hidden", backgroundColor:"red",display:"flex", flexDirection:"column", width:"10%", justifyContent:"flex-start", top: 70}}>
+            <h3>Menu</h3>
+            <button style={{marginBottom:"5px"}}>Home</button>
+            <button style={{marginBottom:"5px"}}>Add Recipe</button>
+            <button style={{marginBottom:"5px"}}>About</button>
         </div>
     )
 }
