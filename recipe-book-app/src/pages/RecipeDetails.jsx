a32290cf9f3b11b4d8e41487dcd65cc6740135c0
Miguel Henriques <miguelaghenriques@gmail.com>
2024-02-09 17:52:33 +0000

    small changes

--- recipe-book-app/src/pages/RecipeDetails.jsx
+++ recipe-book-app/src/pages/RecipeDetails.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
 import kcal from "../assets/images/kcal.png";
 import servings from "../assets/images/servings.png"
 
@@ -14,15 +14,20 @@ function RecipeDetails(props) {
 
     return (
         <section>
-            {
+            <div>
+                {
                 thisRecipe ? (
                 <div key={thisRecipe.id} style={{margin: 20, border:"2px solid black", padding: 50 }}>
                     <h3>{thisRecipe.name}</h3>
                     <img src={thisRecipe.image} alt={thisRecipe.name} width={450} height={400} />
                     <h4><img src={kcal} width={20}height={20} /> Calories: {thisRecipe.calories} {thisRecipe.calories >= 175 && <span style={{backgroundColor: "lightGrey", padding: "5px",borderRadius: 5}}>🔥 High Calories</span>}{thisRecipe.calories < 90 && <span style={{backgroundColor: "lightGrey", padding: "5px",borderRadius: 5}}>🥬 Low Calories  </span>}</h4>
                     <h4><img src={servings} width={20}height={20} /> Servings: {thisRecipe.servings}</h4>
+                    <NavLink to="/">
+                        <button>Back</button>
+                    </NavLink>
                 </div> ) : (<p>No recipe found</p>)
-            }
+                }
+            </div>
         </section>
     )
 }
