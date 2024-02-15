4d2d54e9430cf7c2ff054743d263b9dbd452668b
Daniel Amaral <dfbamaral@gmail.com>
2024-02-15 17:30:04 +0000

    about bnner

--- recipe-book-app/src/components/Navbar.jsx
+++ recipe-book-app/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
-
+import banner from "../assets/images/banner.png"
 function Navbar (props) {
 
     //should display the App's name and logo
 
     return (
-        <header id="header" style={{width: "100%", position: "fixed", top: 0, left: 0, padding: "10px 0", backgroundColor: "orange", display:"flex", justifyContent: "center", alignItems: "center", height: 50}}>
-            <h1>Recipe Book APP</h1><img src="" alt="app logo"></img>
+        <header id="header" style={{width: "100%", position: "fixed", top: 0, left: 0, padding: "10px 0", display:"flex", justifyContent: "center", alignItems: "center", height: 50, backgroundColor: "orange"}}>
+             {/* <img src={banner} alt="app logo" position="fixed" ></img> */} 
+            {/* <img src="../assets/banner.png" alt="" /> */}
         </header>
     )
     
