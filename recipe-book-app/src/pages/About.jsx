2a93cb6e912a528ea083df50f405258ed2c55cad
Miguel Henriques <miguelaghenriques@gmail.com>
2024-02-15 17:39:02 +0000

    link about buttons

--- recipe-book-app/src/pages/About.jsx
+++ recipe-book-app/src/pages/About.jsx
@@ -11,12 +11,12 @@ function About() {
                     <p>Daniel Amaral</p>
                     <div class="buttondan">
                     <button class="button">
-                        <a href="https://www.linkedin.com/in/daniel-amaral-6b077445/" class="no-link-style">
+                        <a href="https://www.linkedin.com/in/daniel-amaral-6b077445/" class="no-link-style" target="_blank">
                         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/2048px-LinkedIn_icon.svg.png" width="65" height="65"   transform="matrix(1, 0, 0, 1, -7.105427357601002e-15, -3.552713678800501e-15)" />
                     </a></button>
                     
                     <button class="button">
-                        <a href="https://github.com/dfbamaral" class="no-link-style">
+                        <a href="https://github.com/dfbamaral" class="no-link-style" target="_blank">
                         <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" width="65" height="65"   transform="matrix(1, 0, 0, 1, -7.105427357601002e-15, -3.552713678800501e-15)" />
                     </a></button>
                     
@@ -27,12 +27,12 @@ function About() {
                     <p>Miguel Henriques</p>
                     <div class="buttonmig">
                     <button class="button">
-                        <a href="http:" class="no-link-style">
+                        <a href="https://www.linkedin.com/in/miguel-henriques-525514177/" class="no-link-style" target="_blank">
                         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/2048px-LinkedIn_icon.svg.png" width="65" height="65"   transform="matrix(1, 0, 0, 1, -7.105427357601002e-15, -3.552713678800501e-15)" />
                     </a></button>
                     
                     <button class="button">
-                        <a href="https://github.com/Miguel-a-Henriques" class="no-link-style">
+                        <a href="https://github.com/Miguel-a-Henriques" class="no-link-style" target="_blank">
                         <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" width="65" height="65"   transform="matrix(1, 0, 0, 1, -7.105427357601002e-15, -3.552713678800501e-15)" />
                     </a></button>
                     
